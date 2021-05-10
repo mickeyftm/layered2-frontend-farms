558cf5165155c99466c1a7cd1d1fb5affdb63eaa
Jason <jasonang0210@gmail.com>
2021-05-10 18:45:23 +0800

    changes

--- src/state/farms/fetchFarmUser.ts
+++ src/state/farms/fetchFarmUser.ts
@@ -65,7 +65,7 @@ export const fetchFarmUserEarnings = async (account: string) => {
   const calls = farmsConfig.map((farm) => {
     return {
       address: masterChefAdress,
-      name: 'pendingMew',
+      name: 'pendingKirby',
       params: [farm.pid, account],
     }
   })
--- src/state/farms/fetchFarms.ts
+++ src/state/farms/fetchFarms.ts
@@ -103,7 +103,7 @@ const fetchFarms = async () => {
         },
         {
           address: getMasterChefAddress(),
-          name: 'mewPerBlock',
+          name: 'kirbyPerBlock',
         },
       ])
 
