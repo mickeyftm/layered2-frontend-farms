e41cc69cb0e679f02c01121acc8c7ea97fbda8e7
Jason <jasonang0210@gmail.com>
2021-05-14 19:35:04 +0800

    'changes'

--- src/views/Farms/Farms.tsx
+++ src/views/Farms/Farms.tsx
@@ -113,7 +113,7 @@ const Farms: React.FC<FarmsProps> = (farmsProps) => {
           </Route>
         </FlexLayout>
       </div>
-      <Image src="https://deathnotefinance.com/images/background.png" width={475} height={295} responsive />
+      <Image src="https://deathnotefinance.com/images/background.png" width={8406} height={4784} responsive />
     </Page >
   )
 }
--- src/views/Referral/Referral.tsx
+++ src/views/Referral/Referral.tsx
@@ -57,7 +57,7 @@ const Referral: React.FC = () => {
                     </CardBody>
                 </StyledCard>
             </Con>
-            <Image src="https://deathnotefinance.com/images/background.png" width={475} height={295} responsive />
+            <Image src="https://deathnotefinance.com/images/background.png" width={8406} height={4784} responsive />
         </Page>
     )
 }
