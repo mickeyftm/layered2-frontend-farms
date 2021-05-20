38b01f32139e7210838a5a3d2858586d47ba8fc8
mrjunying98 <82926228+mrjunying98@users.noreply.github.com>
2021-05-21 01:44:44 +0800

    changes

--- src/state/farms/fetchFarmUser.ts
+++ src/state/farms/fetchFarmUser.ts
@@ -65,7 +65,7 @@ export const fetchFarmUserEarnings = async (account: string) => {
   const calls = farmsConfig.map((farm) => {
     return {
       address: masterChefAdress,
-      name: 'pendingbear2',
+      name: 'pendingBear2',
       params: [farm.pid, account],
     }
   })
